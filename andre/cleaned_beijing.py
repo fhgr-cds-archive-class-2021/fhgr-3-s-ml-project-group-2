645c6ca9bdb671da593fef988975e989ba67376b
André Glatzl <andre@alpinesoft.ch>
2022-11-04 10:30:04 +0000

    update

--- andre/cleaned_beijing.py
+++ andre/cleaned_beijing.py
@@ -5,12 +5,18 @@ from sklearn.pipeline import Pipeline
 from libs import transformers as andre_transformers
 
 df = pd.read_csv('../beijing.csv', encoding="gbk")
+df.drop(columns=['id', 'url'], inplace=True)
+
 label = "totalPrice"
 X_train, X_test, Y_train, Y_test = train_test_split(df.drop(label, axis=1), df[label], test_size=0.2, random_state=42)
 pipe = Pipeline([
     ('preprocessing', andre_transformers.get_preprocessing()),
 ])
-X_train = pipe.fit_transform(X_train)
-X_test = pipe.fit_transform(X_test)
+X_train_transformed = pipe.fit_transform(X_train)
+X_test_transformed = pipe.fit_transform(X_test)
 
-print(len(X_train), Y_train.count())
+# pipeline to dataframe
+X_train_d = pd.DataFrame(X_train_transformed)
+
+print(X_train_d)
+print(X_train)
