c3c2fb979542890572d7fa6e105e916b20b1176c
Deadeye111 <leandro01@bluewin.ch>
2022-11-23 00:08:19 +0100

    leandro: updated /app

--- app/_03_modelling.py
+++ app/_03_modelling.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
 def modelling(train: pd.DataFrame, test: pd.DataFrame) -> pd.DataFrame:
+
+    # define X_train, X_test, y_train, y_test
+    colsToUse = train.columns.to_list()  # all columns
+    colsToUse.remove('totalPrice')  # remove target variable (predicting variable)
+    X_train = train[colsToUse]
+    X_test = test[colsToUse]
+    y_train = train["totalPrice"]
+    y_test = test["totalPrice"]
+    
+
     return None
--- app/test.py
+++ app/test.py
@@ -7,5 +7,17 @@ import numpy as np
 pre_cleaned = pd.read_csv(os.path.join("data", "01_pre_cleaned.csv"), sep=';')
 #print(pre_cleaned.isnull().sum())
 
-test = createYearPeriods(pre_cleaned, columnName="constructionTime")
-print(test.constructionTimePeriod)
+train = pd.read_csv('data/02_train.csv', sep=';')
+test = pd.read_csv('data/02_test.csv', sep=';')
+
+# define X_train, X_test, y_train, y_test
+colsToUse = train.columns.to_list()  # all columns
+colsToUse.remove('totalPrice')  # remove target variable (predicting variable)
+X_train = train[colsToUse]
+X_test = test[colsToUse]
+y_train = train["totalPrice"]
+y_test = test["totalPrice"]
+print("Shape X_train:", X_train.shape)
+print("Shape X_test:", X_test.shape)
+print("Shape y_train:", y_train.shape)
+print("Shape y_test:", y_test.shape)
